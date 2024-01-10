ce2cee582cde91c3b8a2eb24f45641de8041635b
Marylene Mbugua <123755478+Marylene78@users.noreply.github.com>
2024-01-10 08:45:00 +0300

    Add files via upload

---
+++ login-1.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<HTml>
+    <head>
+        <title>login page</title>
+        <link rel="stylesheet" href="login.css">
+    </head>
+    <body>
+<div id="form">
+<form action="login 2.php" method="POST">
+<h1>Registration</h1>
+<p>
+    <label>Name:</label>
+    <input type="text" id="name" name="name"/>
+</p>
+<p>
+    <label>Email:</label>
+    <input type="Email" id="email" name="email"/>
+</p>
+<p>
+    <label>website</label>
+    <input type="website" id="website" name="website"/>
+</p>
+<p>
+    <input type="submit" id="btn" value="login"/>
+</p>
+</form>
+</div>
+    </body>
+</HTml>
